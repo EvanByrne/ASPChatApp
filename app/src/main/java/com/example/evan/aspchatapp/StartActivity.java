00a3f2df373558d68dfe8d17bdebc770735daf2a
Evan <evanbyrne89@gmail.com>
2018-04-11 21:19:13 +0100

    Connected to Firebase Auth, Real Time Databse and FCM also added Splash and test MainActivity

--- app/src/main/java/com/example/evan/aspchatapp/StartActivity.java
+++ app/src/main/java/com/example/evan/aspchatapp/StartActivity.java
@@ -1,5 +1,6 @@
 package com.example.evan.aspchatapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,28 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Thread thread = new Thread()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    sleep(2000);
+                } catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+
+                finally
+                {
+                    Intent i = new Intent(StartActivity.this, MainActivity.class);
+                    startActivity(i);
+                }
+
+            }
+        };
+        thread.start();
     }
 }
